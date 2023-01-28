1c3b111e727f8a75d01ea0eb5fdc9d0d65f6b2cf
Alexman2505 <alexman2505@gmail.com>
2023-01-28 20:20:19 +0300

    release select_related one more time

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,7 +5,8 @@ from .models import Group, Post
 
 
 def index(request):
-    posts = Post.objects.all()[:settings.NUMBER_OF_POSTS]
+    posts = Post.objects.select_related(
+        'group', 'author')[:settings.NUMBER_OF_POSTS]
     context = {
         'posts': posts,
     }
@@ -15,7 +16,7 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.select_related(
-        'group')[:settings.NUMBER_OF_POSTS]
+        'author')[:settings.NUMBER_OF_POSTS]
     context = {
         'group': group,
         'posts': posts,
