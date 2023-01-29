5203b0e79288e74b9b0ac181d56a030047449f02
Alexman2505 <alexman2505@gmail.com>
2023-01-29 11:19:06 +0300

    delete comment in the urls.py and delete the context in the views.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -7,18 +7,16 @@ from .models import Group, Post
 def index(request):
     posts = Post.objects.select_related(
         'group', 'author')[:settings.NUMBER_OF_POSTS]
-    context = {
-        'posts': posts,
-    }
-    return render(request, 'posts/index.html', context)
+    return render(request,
+                  'posts/index.html',
+                  {'posts': posts})
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.select_related(
         'author')[:settings.NUMBER_OF_POSTS]
-    context = {
-        'group': group,
-        'posts': posts,
-    }
-    return render(request, 'posts/group_list.html', context)
+    return render(request,
+                  'posts/group_list.html',
+                  {'group': group,
+                   'posts': posts})
